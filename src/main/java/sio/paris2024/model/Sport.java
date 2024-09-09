f0f40fe1611f9ac62225e791c6ac3c2a99715c61
lebrun <thomas.lebrun7210@gmail.com>
2024-09-09 13:37:35 +0200

    Création de Sport.java

---
+++ src/main/java/sio/paris2024/model/Sport.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sio.paris2024.model;
+
+/**
+ *
+ * @author SIO2
+ */
+public class Sport {
+    
+    private int id;
+    private String nom ;
+    
+     public Sport() {
+    }
+
+    public Sport(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+    
+     
+}
